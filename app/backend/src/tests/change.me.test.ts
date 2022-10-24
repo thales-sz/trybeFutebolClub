02d3b733b422c982eff801d983de2988d296412f
Thales <thales.souz@outlook.com>
2022-10-24 16:56:02 -0300

    test: teste caso de erro em senha ou email

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -15,11 +15,6 @@ const { expect } = chai;
 
 describe('Testa App', () => {
   describe('Quando a req é feita com sucesso', () => {
-    it('a rota /ping deve retornar um status 200 como response e "pong" na messagem', async () => {
-      const httpResponse = await chai.request(app).get('/ping');
-      expect(httpResponse.status).to.equal(StatusCodes.OK);
-      expect(httpResponse.body).to.deep.equal({ message: 'pong' });
-    })
     it('a rota /login deve retornar um status 200 como response e um token válido na messagem', async () => {
       const httpResponseLogin = await chai
       .request(app)
@@ -27,7 +22,16 @@ describe('Testa App', () => {
       .send({ email: 'admin@admin.com', password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW' });
 
       expect(httpResponseLogin.status).to.equal(StatusCodes.OK);
-      expect(httpResponseLogin.body.token.split('E2NjY0')[0]).to.equal("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInVzZXJuYW1lIjoiQWRtaW4iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOj");
+    })
+  })
+  describe('Quando a req NÃO é feita com sucesso', () => {
+    it('deve retornar UNAUTHORIZED caso a senha ou email estejam errados', async () => {
+      const httpResponseSenha = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'email@errado.com', password: 'senhaaaa' });
+
+      expect(httpResponseSenha.status).to.equal(StatusCodes.UNAUTHORIZED);
     })
   })
 });
