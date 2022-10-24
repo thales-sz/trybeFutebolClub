7ee02db3b6f5313f2834f46fd0df591068038c65
Thales <thales.souz@outlook.com>
2022-10-24 20:45:16 -0300

    test: teste para a rota /teams

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,48 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import { StatusCodes } from 'http-status-codes';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+import { Response } from 'superagent';
+import { ITeam } from '../domain/entities/Team';
+import TeamModel from '../database/models/team.model';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const mock: ITeam = {
+  id: 1,
+  teamName: 'Trybe Futebol CLub',
+}
+
+const teams: ITeam[] = [mock, mock, mock];
+
+describe('Testando rota /teams', () => {
+  afterEach(() => { sinon.restore() });
+
+  it('deve retornar um array de teams na rota /teams', async () => {
+    sinon.stub(TeamModel, 'findAll').resolves(teams as Array<TeamModel>);
+
+    const httpResponse = await chai.request(app).get('/teams');
+
+    expect(httpResponse.status).to.be.equal(200);
+    expect(httpResponse.body[0]).to.haveOwnProperty('id');
+  });
+
+  it('deve retornar apenas um time na rota /teams/:id', async () => {
+    sinon
+    .stub(TeamModel, 'findOne')
+    .resolves(mock as TeamModel);
+
+    const httpResponse = await chai.request(app).get('/teams/1');
+
+    expect(httpResponse.status).to.be.equal(200);
+    expect(httpResponse.body).to.haveOwnProperty('id');
+  });
+
+  
+});
