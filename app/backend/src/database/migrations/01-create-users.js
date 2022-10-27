5a5a1815775ae9ebcba2001521931ab2f791ecc9
Thales <thales.souz@outlook.com>
2022-10-27 20:48:16 -0300

    fix: deploy

--- app/backend/src/database/migrations/01-create-users.js
+++ app/backend/src/database/migrations/01-create-users.js
@@ -1,32 +1,35 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+    await queryInterface.createTable(
+      'users',
+      {
+        id: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        username: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        role: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        email: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        password: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
       },
-      username: {
-        allowNull: false,
-        type: Sequelize.STRING
+      {
+        timestamps: false,
       },
-      role: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      email: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      password: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      }
-    },
-    {
-      timestamps: false,
-    })
+    );
   },
   down: async (queryInterface) => {
     await queryInterface.dropTable('users');
